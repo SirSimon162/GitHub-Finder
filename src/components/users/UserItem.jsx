dede5bc3a0eda4dc7c109e8041a00f7cdb4b4230
SirSimon162 <anmolpattnayak2016@gmail.com>
2022-12-26 03:00:39 +0530

    styling changes

--- src/components/users/UserItem.jsx
+++ src/components/users/UserItem.jsx
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom'
 
 function UserItem({user: {login, avatar_url}}) {
   return (
-    <div className='card shadow-md compact side bg-base-300 border-r-4'>
-        <div className='flex-row items-center space-x-4 card-body'>
+    <div className='card shadow-md compact side bg-base-300 border-b-4 border-neutral-content'>
+        <div className='flex-col items-center card-body '>
             <div>
                 <div className='avatar'>
                     <div className="rounded-full shadow w-14 h-14">
@@ -12,9 +12,9 @@ function UserItem({user: {login, avatar_url}}) {
                     </div>
                 </div>
             </div>
-            <div>
+            <div className='flex flex-col items-center justify-center'>
                 <h2 className='card-title'>{login}</h2>
-               <Link className='text-base-content text-opacity-40' to={`/user/${login}`}>Visit Profile</Link> 
+               <Link className='text-base-content text-opacity-40 btn btn-xs mt-1 border-base-300' to={`/user/${login}`}>Visit Profile</Link> 
             </div>
         </div>
     </div>
