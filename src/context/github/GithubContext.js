31b2ab4f1e46d4ccd8d09e55c9ccb48989ceadfc
SirSimon162 <anmolpattnayak2016@gmail.com>
2022-12-26 01:57:42 +0530

    User profile section added

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -9,6 +9,7 @@ export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
     user: {},
+    repos: [],
     loading: false
   }
 
@@ -52,6 +53,24 @@ export const GithubProvider = ({ children }) => {
     }
   };
 
+  const getUserRepos = async (login) => {
+    setLoading()
+    const params = new URLSearchParams({
+      sort:'created',
+      per_page: 10
+    })
+    const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`, {
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`,
+      },
+    });
+    const data = await response.json();
+    dispatch({
+      type: 'GET_REPOS',
+      payload: data,
+    })
+  };
+
   const clearUsers = () => dispatch({type: 'CLEAR_USERS'})
 
   const setLoading = () => dispatch({type:'SET_LOADING'})
@@ -62,9 +81,11 @@ export const GithubProvider = ({ children }) => {
         users: state.users,
         loading: state.loading,
         user: state.user,
+        repos: state.repos,
         searchUsers,
         clearUsers,
-        getUser
+        getUser,
+        getUserRepos,
       }}
     >
       {children}
